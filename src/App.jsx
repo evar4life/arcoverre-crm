81994fe5d2af885c3b56d8d029e989224fa7d960
evar4life <evar4life@gmail.com>
2026-04-26 17:37:51 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import { useEffect, useState } from "react";
-import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
 );
 
 const leadStatuses = [
